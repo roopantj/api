96c8a895fa826f920b9ea6632e81a5265e0be3bf
roopantj <ROOPANTJ7@GMAIL.COM>
2021-09-02 23:10:33 +0530

    Error Handler

--- api/src/App.js
+++ api/src/App.js
@@ -5,25 +5,39 @@ const App = () =>  {
     
    const [movies, setMovies] = useState([])
    const [Loader,setLoader] =useState(false)
-   const fetchDataHanlder = () =>{
-    setLoader(true) 
-     fetch('https://swapi.dev/api/films').then(response => response.json()).then(data=>{
-     setMovies(data.results)
-     setLoader(false)
-     console.log(movies)
-    })
+   const [error,setError] = useState(null)
    
-  }
+   const  fetchDataHandler = async () =>{
+     if(movies.length===0){
+          try{
+              setLoader(true) 
+              const response = await fetch('https://swapi.dev/api/film')
+              if(!response.ok)
+                throw new Error(`Something went wrong! Status Code :${response.status}`)
+              const data = await response.json()
+              setMovies(data.results)
+          }
+          catch(error){
+            setError(error.message)
+          }
+          setLoader(false)
+     }
+   }
+   
+  let content = <p>No movies. Hit Get Movies </p>
+  if(error)
+    content=<h3>{error}</h3> 
+  if(Loader)
+    content=<h3>Loading ...</h3>
+  if(movies.length>0)
+    content=movies.map((movie=><h3>{movie.title}</h3>))  
 
   return (
+   
     <div className="App">
       <h3>Movies list</h3>
-       <button onClick={fetchDataHanlder}>Get data</button>
-        {
-          movies && movies.map(movie=><h3>{movie.title}</h3>)}
-          {
-          Loader && <h1>Loading...</h1> 
-        }
+       <button onClick={fetchDataHandler}>Get Movies</button>
+       {content}
     </div>
   );
 }
